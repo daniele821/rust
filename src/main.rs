135acfb6238967820e28b4747ec50434e72dff19
Daniele Muffato <danixgithub1@gmail.com>
2025-03-26 22:22:02 +0100

    convert from and back time

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use chrono::{Local, TimeZone};
+use chrono::{Local, NaiveDateTime, TimeZone};
 
 fn main() {
     // Get current time as Unix timestamp
@@ -8,8 +8,18 @@ fn main() {
     let datetime = Local.timestamp_opt(now, 0).unwrap();
 
     // Format as 'YYYY/MM/DD hh:mm'
-    let formatted_time = datetime.format("%Y/%m/%d %H:%M").to_string();
+    let formatted_time = datetime.format("%Y/%m/%d %H:%M:%S").to_string();
 
     // Print result
     println!("time:{now} | {formatted_time}");
+
+    let naive_dt = NaiveDateTime::parse_from_str(&formatted_time, "%Y/%m/%d %H:%M:%S").unwrap();
+
+    // Convert to Unix timestamp using the local timezone
+    let unix_time = Local.from_local_datetime(&naive_dt).unwrap().timestamp();
+
+    // Print Unix timestamp
+    println!("{unix_time}");
+
+    assert_eq!(unix_time, now);
 }
