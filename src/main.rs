9c4241f8cf27b6fcc57639c291f01f7e0ba9d0bc
Daniele Muffato <danixgithub1@gmail.com>
2025-03-27 03:20:47 +0100

    add i64 -> datetime

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,6 @@ use chrono::Local;
 fn main() {
     let naive = Local::now().naive_local().and_utc().timestamp(); // Remove timezone info
 
-    println!("Unix timestamp (UTC): {naive}");
+    let datetime = chrono::DateTime::from_timestamp(naive, 0);
+    println!("Unix timestamp (UTC): {naive} {datetime:?}");
 }
-
