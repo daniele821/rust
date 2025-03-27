5f665bcd639df851c15502a5edc199bfb83c4202
Daniele Muffato <danixgithub1@gmail.com>
2025-03-27 03:35:26 +0100

    fmt

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
-use chrono::Local;
+use chrono::{DateTime, Local, NaiveDateTime};
 
 fn main() {
     let naive = Local::now().naive_local().and_utc().timestamp(); // Remove timezone info
 
     let datetime = chrono::DateTime::from_timestamp(naive, 0);
     println!("Unix timestamp (UTC): {naive} {datetime:?}");
-}
 
+    let fmt = DateTime::from_timestamp(naive, 0)
+        .unwrap()
+        .format("%Y/%m/%d %H:%M")
+        .to_string();
+    println!("{fmt:?}");
+}
