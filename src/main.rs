38407d7a931f552a85f4b969d56f37ca63a7110d
Daniele Muffato <danixgithub1@gmail.com>
2025-04-02 20:04:48 +0200

    fmt test

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,57 @@
 use crossterm::{
-    event::{DisableMouseCapture, EnableMouseCapture},
+    event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode, KeyEvent},
     execute,
     terminal::{
-        DisableLineWrap, EnableLineWrap, EnterAlternateScreen, LeaveAlternateScreen,
-        disable_raw_mode, enable_raw_mode,
+        Clear, ClearType, DisableLineWrap, EnableLineWrap, EnterAlternateScreen,
+        LeaveAlternateScreen, disable_raw_mode, enable_raw_mode, size,
     },
 };
-use std::{io, thread::sleep, time::Duration};
+use std::{
+    io::{self, Write},
+    time::Duration,
+};
+
+fn draw<W: Write>(w: &mut W) -> io::Result<()> {
+    let (cols, rows) = size()?;
+    execute!(w, Clear(ClearType::All))?;
+    for y in 0..rows {
+        for x in 0..cols {
+            write!(w, "#")?;
+        }
+        if y == rows - 1 {
+            write!(w, "\r")?;
+        } else {
+            write!(w, "\r\n")?;
+        }
+    }
+    w.flush()
+}
 
 fn main() -> io::Result<()> {
     enable_raw_mode()?;
     execute!(
-        std::io::stdout(),
+        io::stdout(),
         EnterAlternateScreen,
         EnableMouseCapture,
         DisableLineWrap,
     )?;
 
-    sleep(Duration::from_secs(3));
+    let mut stdout = io::stdout();
+    draw(&mut stdout)?;
+
+    loop {
+        if event::poll(Duration::from_millis(100))? {
+            match event::read()? {
+                Event::Key(KeyEvent {
+                    code: KeyCode::Esc, ..
+                }) => break,
+                Event::Resize(_, _) => {
+                    draw(&mut stdout)?;
+                }
+                _ => {}
+            }
+        }
+    }
 
     execute!(
         io::stdout(),
