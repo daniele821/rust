ae0b534ec83d42d3d65f229b301dda85cf40fea1
Daniele Muffato <danixgithub1@gmail.com>
2025-04-15 10:28:32 +0200

    removed stuff

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,3 @@
-use anyhow::{Result, anyhow};
-
-fn main() -> Result<()> {
-    Err(anyhow!("Missing attribute: {}", 12))
-    // let path = "/home/daniele/.bash_history";
-    //
-    // let content =
-    //     std::fs::read(path).with_context(|| format!("Failed to read instrs from {}", path))?;
-    // Ok(())
+fn main() {
+    println!("test");
 }
